d2c18444643b631d326f910f4dee7ac459a78a68
mavcycyan <pbl6ewka@gmail.com>
2019-07-13 09:00:36 +0300

    - добавлено раскрытие меню на мобильной версии, добавлено открытие модалки обратного звонка

--- js/main.js
+++ js/main.js
@@ -1,41 +1,9 @@
 /**/
 jQuery(document).ready(function($){	
-    $(".ath").fancybox({ 
-        href: '#modal_auth', 
-        wrapCSS: 'modal_auth_wp',
-        minWidth: '100%',
-        margin: [0, 0, 0, 0],
-        padding: [0, 0, 0, 0],
-        autoScale: true
-    });
-    $(".fp").fancybox({ 
-        href: '#modal_fp', 
-        wrapCSS: 'modal_auth_wp',
-        minWidth: '100%',
-        margin: [0, 0, 0, 0],
-        padding: [0, 0, 0, 0],
-        autoScale: true
-    });
-    $(".mn").fancybox({ 
-        href: '#modal_mn', 
-        wrapCSS: 'modal_auth_wp',
-        minWidth: '100%',
-        margin: [0, 0, 0, 0],
-        padding: [0, 0, 0, 0],
-        autoScale: true
-    });
+$(".header_text").fancybox({ href: '#modal_auth', wrapCSS: 'modal_call_back'});
 });
 /**/
 
-/**/
-$(window).on('load', function () {
-    var $preloader = $('#p_prldr'),
-        $svg_anm   = $preloader.find('.svg_anm');
-    $svg_anm.fadeOut();
-    $preloader.delay(1000).fadeOut('slow');
-    $('#svg_pr rect').animate({ width: 526 }, 1000, function() { });
-});
-/**/
 
 /**/
 /**/
